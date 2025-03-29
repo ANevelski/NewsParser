20b25d835a5e71292bb8a92a2f7a195ced3a4c00
AlakseiNevelski <Aliaksei_Nevelski@epam.com>
2025-03-29 14:39:30 +0100

    Minor changes in the naming

--- create_articals_with_summary_and_topics_client.py
+++ create_articals_with_summary_and_topics_client.py
@@ -5,9 +5,9 @@ import create_openip_client
 def summarize_and_topics_for_artical(articles):
     # Looping through a list of objects (JSON array) to add summary and topics
     client = create_openip_client.create_openip_client()
-    for item in articles:  
+    for article in articles:  
         # Returs JSON {"summary" : "summary text", "topics": ["topics array"]}  
-        summary_topics = summarize_and_topics_client.summarize_and_get_topics_content(client, item['content'])        
-        item["summary"] = summary_topics["summary"]    
-        item["topics"] = summary_topics["topics"]
+        summary_topics = summarize_and_topics_client.summarize_and_get_topics_content(client, article['content'])        
+        article["summary"] = summary_topics["summary"]    
+        article["topics"] = summary_topics["topics"]
             
